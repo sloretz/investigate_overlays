575d07f9823ed80e7679022f49b72e6676154346
Shane Loretz <sloretz@osrfoundation.org>
2021-09-14 13:26:57 -0700

    Add example showing static libs not working in non-leaf case
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

---
+++ static_libs/overlay/src/pkg_a/include/pkg_a.h
@@ -0,0 +1,14 @@
+#ifndef pkg_a__pkg_a_H_
+#define pkg_a__pkg_a_H_
+
+#include <string>
+
+namespace pkg_a {
+class A
+{
+public:
+  std::string say_something() const;
+};
+}
+
+#endif  // pkg_a__pkg_a_H_
---
+++ static_libs/underlay/src/pkg_b/include/pkg_b.h
@@ -0,0 +1,15 @@
+#ifndef pkg_b__pkg_b_H_
+#define pkg_b__pkg_b_H_
+
+#include <string>
+
+namespace pkg_b {
+class B
+{
+public:
+  std::string what_does_A_say() const;
+};
+}
+
+#endif  // pkg_b__pkg_b_H_
+
